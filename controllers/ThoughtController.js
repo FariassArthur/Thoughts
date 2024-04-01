f521e212bda474dd5a5beb9357485d2a6d2b5b65
arthurfarias <arthurfarias@arthurfarias.fariasarthur>
2024-04-01 19:38:41 -0300

    fix dashboard view

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -6,11 +6,46 @@ module.exports = class ThoughtController {
     res.render("thoughts/home");
   }
 
-  static async dashboard (req, res) {
-    res.render('thoughts/dashboard')
+  static async dashboard(req, res) {
+    const userId = req.session.userid;
+
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Thought,
+      plain: true,
+    });
+
+    if(!user) {
+      res.redirect('/login')
+    }
+
+    const thoughts = user.Thoughts.map((result) => result.dataValues)
+
+    res.render("thoughts/dashboard", {thoughts});
   }
 
-  static createThought (req, res) {
-    res.render('thoughts/create')
+  static createThought(req, res) {
+    res.render("thoughts/create");
+  }
+
+  static async createThoughtSave(req, res) {
+    const thought = {
+      title: req.body.title,
+      UserId: req.session.userid,
+    };
+
+    try {
+      await Thought.create(thought);
+
+      req.flash("message", "Pensamento criado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/thoughts/dashboard");
+      });
+    } catch (err) {
+      console.log(`Aconteceu um error: ${err}`);
+    }
   }
 };
